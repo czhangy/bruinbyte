9d9d657c83aaeda4c6ee5f81b7c23c40f9c19763
Victoria Delk <vdelkpictures@gmail.com>
2020-11-25 22:27:38 -0800

    outline of how comments are gonna work and idea of how to change asthetics on card

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -1,8 +1,30 @@
 import React from "react";
+
+////card shit
+//import { makeStyles } from "@material-ui/core/styles";
+//import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+//import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+/////
+/////
+////end card shit
+
 // nodejs library that concatenates classes
-import classNames from "classnames";
+//import classNames from "classnames";
 // react components for routing our app without refresh
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
@@ -11,22 +33,22 @@ import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
+//import Button from "components/CustomButtons/Button.js";
 import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
-import SectionBasics from "./Sections/SectionBasics.js";
-import SectionNavbars from "./Sections/SectionNavbars.js";
-import SectionTabs from "./Sections/SectionTabs.js";
-import SectionPills from "./Sections/SectionPills.js";
-import SectionNotifications from "./Sections/SectionNotifications.js";
-import SectionTypography from "./Sections/SectionTypography.js";
-import SectionJavascript from "./Sections/SectionJavascript.js";
-import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
-import SectionLogin from "./Sections/SectionLogin.js";
-import SectionExamples from "./Sections/SectionExamples.js";
-import SectionDownload from "./Sections/SectionDownload.js";
+// import SectionBasics from "./Sections/SectionBasics.js";
+// import SectionNavbars from "./Sections/SectionNavbars.js";
+// import SectionTabs from "./Sections/SectionTabs.js";
+// import SectionPills from "./Sections/SectionPills.js";
+// import SectionNotifications from "./Sections/SectionNotifications.js";
+// import SectionTypography from "./Sections/SectionTypography.js";
+// import SectionJavascript from "./Sections/SectionJavascript.js";
+// import SectionCarousel from "./Sections/SectionCarousel.js";
+// import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
+// import SectionLogin from "./Sections/SectionLogin.js";
+// import SectionExamples from "./Sections/SectionExamples.js";
+// import SectionDownload from "./Sections/SectionDownload.js";
 
 import styles from "assets/jss/material-kit-react/views/components.js";
 
@@ -53,17 +75,72 @@ export default function Components(props) {
           <GridContainer>
             <GridItem>
               <div className={classes.brand}>
-                <h1 className={classes.title}>Material Kit React.</h1>
+                <h1 className={classes.title}>BruinByte.</h1>
                 <h3 className={classes.subtitle}>
-                  A Badass Material-UI Kit based on Material Design.
+                  Like Yelp but better and Bruin
                 </h3>
+                <h4 className={classes.subtitle}>gettin egged today</h4>
               </div>
             </GridItem>
           </GridContainer>
         </div>
       </Parallax>
 
-      <div className={classNames(classes.main, classes.mainRaised)}>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="this is where comments are gonna go"
+          subheader="September 14, 2016 --> data???"
+        />
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/paella.jpg"
+          title="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            not sure whats gonna go here but comment stuff
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+          // className={clsx(classes.expand, {
+          //   [classes.expandOpen]: expanded,
+          // })}
+          // onClick={handleExpandClick}
+          // aria-expanded={expanded}
+          // aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={true} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Name?</Typography>
+            <Typography paragraph>first paragraph comment</Typography>
+            <Typography paragraph>second paragraph comment</Typography>
+            <Typography paragraph>third paragraph comment</Typography>
+            <Typography>4th paragraph comment</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+
+      {/* <div className={classNames(classes.main, classes.mainRaised)}>
         <SectionBasics />
         <SectionNavbars />
         <SectionTabs />
@@ -83,7 +160,7 @@ export default function Components(props) {
         </GridItem>
         <SectionExamples />
         <SectionDownload />
-      </div>
+      </div> */}
       <Footer />
     </div>
   );
